a113e63c25bb298db66be7c54f27fccbafa3e2cc
yunghsiang-chiang <yunghsiang.chiang@hochi.org.tw>
2024-09-20 15:13:37 +0800

    2024/9/20 15:13

--- hochi_food/obj/Release/net8.0/win-x64/hochi_food.AssemblyInfo.cs
+++ hochi_food/obj/Release/net8.0/win-x64/hochi_food.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hochi_food")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+044478788e12b13cd853e1691bb08b1366e326b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8929c2e11d6a80898fdb436877d5e1509a5cb19a")]
 [assembly: System.Reflection.AssemblyProductAttribute("hochi_food")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hochi_food")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
