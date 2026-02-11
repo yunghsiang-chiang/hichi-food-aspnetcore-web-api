a485847967e7402ea0b02712f6965595f771d0be
yunghsiang-chiang <yunghsiang.chiang@hochi.org.tw>
2026-02-11 14:23:57 +0800

    2026/02/11 14:23

--- hochi_food/obj/Debug/net8.0/hochi_food.AssemblyInfo.cs
+++ hochi_food/obj/Debug/net8.0/hochi_food.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hochi_food")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4380aa2f1b95c25d906e907b0dc1bd400150e51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86e6e183877e39c937da0c7b763fd75220364079")]
 [assembly: System.Reflection.AssemblyProductAttribute("hochi_food")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hochi_food")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- hochi_food/obj/Release/net8.0/win-x64/hochi_food.AssemblyInfo.cs
+++ hochi_food/obj/Release/net8.0/win-x64/hochi_food.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hochi_food")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1d90bebf84126ad288f3deafbd6b85a68a26bc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86e6e183877e39c937da0c7b763fd75220364079")]
 [assembly: System.Reflection.AssemblyProductAttribute("hochi_food")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hochi_food")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
